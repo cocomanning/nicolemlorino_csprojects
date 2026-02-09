ad157ee6a1f2d97fe741a97259c9b20910632312
cocomanning <nicolemlorino@gmail.com>
2026-02-08 19:16:17 -0800

    Update townle.py

--- townle.py
+++ townle.py
@@ -1,3 +1,15 @@
+"""
+Welcome to the game "Townle!" This is an online guessing game inspired by Wordle, Globle, and Worldle. 
+Essentially, the player tries to guess the mystery city, with each guess getting a measure of accuracy 
+provided by the system. The player plays "hot or cold" until they have found the mystery city. 
+
+Townle is still being workshopped and only exists as a python terminal game at this point. I am working on 
+developing a website to host the game, complete with an interactive map that updates with each guess and 
+gives the player visual clues regarding their accuracy. Eventually, the mystery city will change every day 
+instead of every round. My goal is to create an assortment of mini games, with each game consisting of cities 
+from different places in the world like Europe, individual U.S. states, and individual countries.
+"""
+
 import random
 
 MASTER_LIST = [['New York City', ' New York', ' 40.7128', ' 74.006'],
